45ca1360f6de575ffe4d23b870d41f28a55582cc
PIYUSH SINGHAL <singhal.piyush2010@gmail.com>
2026-02-18 20:33:45 +0530

    tweaks

--- DATA/rules.js
+++ DATA/rules.js
@@ -2,11 +2,11 @@ export const RULES = `
 +--------------------------------------------------+
 | RULES PROTOCOL                                   |
 +--------------------------------------------------+
-| AI Usage            | Strictly Prohibited       |
-| Version Control     | Mandatory GitHub Usage    |
-| Commit Tracking     | Monitored Throughout      |
-| Team Size           | Max 4 Members             |
-| Duration            | 36 Hours                  |
-| Demo                | Live Final Presentation   |
+| AI Usage            | Strictly Prohibited        |
+| Version Control     | Mandatory GitHub Usage     |
+| Commit Tracking     | Monitored Throughout       |
+| Team Size           | Max 4 Members              |
+| Duration            | 36 Hours                   |
+| Demo                | Live Final Presentation    |
 +--------------------------------------------------+
 `;
